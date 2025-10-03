ac7e7b41711a8a7a98343b2db7775f0eeebbc034
JayantRautela <jayantrautela0205@gmail.com>
2025-10-03 18:28:15 +0530

    changed types

--- apps/http-backend/types/express.d.ts
+++ apps/http-backend/types/express.d.ts
@@ -1,11 +1,11 @@
 import { JwtPayload } from "jsonwebtoken";
 
-declare module "express-server-static-core" {
+declare module "express-serve-static-core" {
     interface Request {
         user?: {
             email: string;
             id: string;
             name: string;
-        } | JwtPayload
+        } & JwtPayload
     }
 }
