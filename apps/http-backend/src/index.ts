fec3837af1adb533009e730b3151cd36a2382d2b
JayantRautela <jayantrautela0205@gmail.com>
2025-10-03 12:22:54 +0530

    feat: add user signup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import UserRouter from "./routes/user.route";
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/user', UserRouter);
 
 app.listen(3001);
