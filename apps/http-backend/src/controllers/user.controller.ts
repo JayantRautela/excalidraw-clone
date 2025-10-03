a01e66268a76c2441b0eac009cf8c8b9ba0795f6
JayantRautela <jayantrautela0205@gmail.com>
2025-10-03 17:55:37 +0530

    feat: add signin endpoint

--- apps/http-backend/src/controllers/user.controller.ts
+++ apps/http-backend/src/controllers/user.controller.ts
@@ -1,7 +1,9 @@
 import { prisma } from "@repo/db/client";
 import { Request, Response, RequestHandler } from "express";
-import { signupSchema } from "@repo/common/schema";
+import { signinSchema, signupSchema } from "@repo/common/schema";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config"
 
 export const signup: RequestHandler = async (req: Request, res: Response) => {
     const { success, data, error } =  signupSchema.safeParse(req.body);
@@ -52,6 +54,69 @@ export const signup: RequestHandler = async (req: Request, res: Response) => {
         res.status(500).json({
             message: "Some Error Occured",
             success: false,
+        });
+        return;
+    }
+}
+
+export const signin: RequestHandler = async (req: Request, res: Response) => {
+    const { success, data, error } = signinSchema.safeParse(req.body);
+
+    if (!success) {
+        console.error(error);
+        res.status(400).json({
+            success: false,
+            message: "input validation error",
+            error: error.issues
+        });
+        return;
+    }
+    try {
+        const { email, password } = data;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (!user) {
+            res.status(404).json({
+                message: "user not found",
+                success: false
+            });
+            return;
+        }
+
+        const userPassword = user.password;
+
+        const isPasswordCorrect = bcrypt.compareSync(password, userPassword);
+
+        if (!isPasswordCorrect) {
+            res.status(403).json({
+                message: "Incorrect Password",
+                success: false
+            });
+            return;
+        }
+
+        const token = jwt.sign({
+            id: user.id,
+            email: user.email,
+            name: user.name
+        }, JWT_SECRET);
+
+        res.status(200).json({
+            message: "User logged in",
+            token: token,
+            success: true
+        });
+        return;
+    } catch (error) {
+        console.error("Some Error Occured :- ", error);
+        res.status(500).json({
+            message: "Some Error Occured",
+            success: false
         });
         return;
     }
