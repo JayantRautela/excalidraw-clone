2633bea238dc572eca13b2f53d6b9ade5e516a87
JayantRautela <jayantrautela0205@gmail.com>
2025-10-05 00:01:43 +0530

    feat: add user route for signin and create-room

--- apps/http-backend/src/routes/user.route.ts
+++ apps/http-backend/src/routes/user.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { signup } from "../controllers/user.controller";
+import { createRoom, signin, signup } from "../controllers/user.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router: Router = Router();
 
 router.post('/signup', signup);
+router.post('/signin', signin);
+router.post('/create-room', authMiddleware, createRoom);
 
 export default router;
