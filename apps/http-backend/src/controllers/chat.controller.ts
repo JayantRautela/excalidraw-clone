e512df53427980eacf2ec3cc68e8af664621da61
JayantRautela <jayantrautela0205@gmail.com>
2025-10-09 18:25:59 +0530

    feat: add controller to get previous chats

---
+++ apps/http-backend/src/controllers/chat.controller.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@repo/db/client";
+import { Request, Response, RequestHandler } from "express";
+
+export const getChats: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const roomId = Number(req.params.roomId);
+
+        if (!roomId) {
+            res.status(400).json({
+                message: "Room id is required",
+                success: false
+            });
+            return;
+        }
+
+        const chats = await prisma.chat.findMany({
+            where: {
+                roomId: roomId
+            },
+            orderBy: {
+                id: "desc"
+            },
+            take: 100
+        });
+
+        res.status(200).json({
+            message: "Previous chats fetched",
+            success: true,
+            chats: chats
+        });
+        return;
+    } catch (error) {
+        console.error("Some Error Occured :-", error);
+        res.status(500).json({
+            message: "Some Error Occured",
+            success: false
+        });
+        return;
+    }
+}
